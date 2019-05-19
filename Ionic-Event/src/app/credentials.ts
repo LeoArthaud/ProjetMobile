656a8e567c36a0a7e96ec4980c8a3e727082fd48
DaceMoria <leo.arthaud@epsi.fr>
2019-05-19 17:38:18 +0200

    Add Full projet

---
+++ Ionic-Event/src/app/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyBPEb6f0WdJ_UXj2ulDsWMNaqU7IGpWWWA',
+    authDomain: 'projetionic-f9dbd.firebaseapp.com',
+    databaseURL: 'https://projetionic-f9dbd.firebaseio.com',
+    projectId: 'projetionic-f9dbd',
+    storageBucket: 'projetionic-f9dbd.appspot.com',
+    messagingSenderId: '785557191427',
+    appId: '1:785557191427:web:07e8f0b4207643ef'
+};
